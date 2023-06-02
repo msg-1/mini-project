1bba423c643ddcd6245317dff16be35683aaa468
unknown <arupaul1650@gmail.com>
2023-06-02 20:44:46 +0530

    second

---
+++ scripts/badminton.js
@@ -0,0 +1,219 @@
+let BadmintonArray = [
+  // badminton 
+  {
+    brand: "Yonex",
+    model: "Voltric Z-Force II",
+    racketWeight: "3U",
+    stringTension: "24-35 lbs",
+    price: 209.99,
+    imageLink: "https://m.media-amazon.com/images/I/617ZfMzNgyL._AC_UF1000,1000_QL80_.jpg"
+  },
+  {
+    brand: "Victor",
+    model: "Brave Axe 1",
+    racketWeight: "3U",
+    stringTension: "24-30 lbs",
+    price: 189.99,
+    imageLink: "https://m.media-amazon.com/images/I/31LtZQ7pNDL._AC_SR300,300.jpg"
+  },
+  {
+    brand: "Li-Ning",
+    model: "Aeronaut 9000C",
+    racketWeight: "3U",
+    stringTension: "24-30 lbs",
+    price: 179.99,
+    imageLink: "https://www.pdhsports.com/files/uk/imagelibrary/Top%2010%20Badminton/2022/RBYO25298~CherrySunburst99Pro4U_P1.jpg"
+  },
+  {
+    brand: "Apacs",
+    model: "Lethal 66",
+    racketWeight: "3U",
+    stringTension: "24-30 lbs",
+    price: 99.99,
+    imageLink: "https://i.pinimg.com/236x/bd/e4/54/bde45400e33a1c769ee925a285014f07.jpg"
+  },
+  {
+    brand: "Victor",
+    model: "Brave Sword 12",
+    racketWeight: "3U",
+    stringTension: "24-30 lbs",
+    price: 189.99,
+    imageLink: "https://m.media-amazon.com/images/I/31LtZQ7pNDL._AC_SR300,300.jpg"
+  },
+  {
+    brand: "Yonex",
+    model: "Duora 10",
+    racketWeight: "3U",
+    stringTension: "24-30 lbs",
+    price: 199.99,
+    imageLink: "https://www.sportsuncle.com/image/cache/catalog/images/lining/rackets/ws72-blk-gold-1200x1200.jpg"
+  },
+  {
+    brand: "Apacs",
+    model: "Lethal 96",
+    racketWeight: "3U",
+    stringTension: "24-30 lbs",
+    price: 99.99,
+    imageLink: "https://i.pinimg.com/236x/bd/e4/54/bde45400e33a1c769ee925a285014f07.jpg"
+  },
+  {
+    brand: "Victor",
+    model: "Brave Sword 19",
+    racketWeight: "3U",
+    stringTension: "24-29 lbs",
+    price: 159.99,
+    imageLink: "https://storage.sg.content-cdn.io/cdn-cgi/image/width=500,height=500,quality=75,format=auto,fit=cover,g=top/in-resources/b368029c-a4dd-448a-a888-58348cb1b144/Images/ProductImages/Source/BB166298.jpg"
+  },
+  {
+    brand: "Yonex",
+    model: "Nanoflare 700",
+    racketWeight: "4U",
+    stringTension: "20-27 lbs",
+    price: 209.99,
+    imageLink: "https://storage.sg.content-cdn.io/cdn-cgi/image/width=500,height=500,quality=75,format=auto,fit=cover,g=top/in-resources/b368029c-a4dd-448a-a888-58348cb1b144/Images/ProductImages/Source/YXNF001AGEFR.jpg"
+  },
+  {
+    brand: "Li-Ning",
+    model: "Windstorm 79",
+    racketWeight: "4U",
+    stringTension: "24-30 lbs",
+    price: 129.99,
+    imageLink: "https://storage.sg.content-cdn.io/cdn-cgi/image/width=500,height=500,quality=75,format=auto,fit=cover,g=top/in-resources/b368029c-a4dd-448a-a888-58348cb1b144/Images/ProductImages/Source/YXNF001FLGN.jpg"
+  },
+  {
+    brand: "Apacs",
+    model: "Feather Weight 59",
+    racketWeight: "4U",
+    stringTension: "24-30 lbs",
+    price: 99.99,
+    imageLink: "https://storage.sg.content-cdn.io/cdn-cgi/image/width=500,height=500,quality=75,format=auto,fit=cover,g=top/in-resources/b368029c-a4dd-448a-a888-58348cb1b144/Images/ProductImages/Source/YXAX88DTGECMGO.jpg"
+  },
+  
+  {
+    brand: "Yonex",
+    model: "Voltric Z-Force",
+    racketWeight: "3U",
+    stringTension: "24-30 lbs",
+    price: 219.99,
+    imageLink: "https://storage.sg.content-cdn.io/cdn-cgi/image/width=500,height=500,quality=75,format=auto,fit=cover,g=top/in-resources/b368029c-a4dd-448a-a888-58348cb1b144/Images/ProductImages/Source/YXNF001FLGO.jpg"
+  },
+  // shuttelcock
+  {
+    brand: "Yonex",
+    model: "AS-30",
+    speed: "Medium",
+    price: 24.99,
+    imageLink: "https://5.imimg.com/data5/HT/YK/PC/SELLER-22616927/aero-speed-3-badminton-shuttlecock.jpg"
+  },
+  {
+    brand: "Victor",
+    model: "Gold Feather",
+    speed: "Fast",
+    price: 19.99,
+    imageLink: "https://www.ycsports.com/image/cache/data/Yonex/AS20-yy-badminton-shuttlecock-light-speed-eather-victor-shuttlecocks-aeroplane-rsl-duck-ball-AS-20-enlan.jpg_640x640-1-640x700.jpg"
+  },
+  {
+    brand: "Li-Ning",
+    model: "A+60",
+    speed: "Medium",
+    price: 16.99,
+    imageLink: "https://cdn.shopify.com/s/files/1/0020/9407/1890/products/c2e76205-a28a-4b7a-9b94-3ec74aa7d7ff_2048x2048.jpg?v=1601305779"
+  },
+  {
+    brand: "RSL",
+    model: "Tourney No. 3",
+    speed: "Slow",
+    price: 14.99,
+    imageLink: "https://5.imimg.com/data5/KW/IG/MY-19880050/a-500x500.jpg"
+  },
+  {
+    brand: "Yonex",
+    model: "Aerosensa 50",
+    speed: "Medium",
+    price: 21.99,
+    imageLink: "https://cdn.shopify.com/s/files/1/0293/4669/3204/products/2061.jpg?v=1669173599"
+  }
+  ,
+  {
+    brand: "Victor",
+    model: "Gold Feather",
+    speed: "Fast",
+    price: 19.99,
+    imageLink: "https://m.media-amazon.com/images/I/6187bQjjEJL.jpg"
+  }
+  ,
+  {
+    brand: "RSL",
+    model: "Tourney No. 3",
+    speed: "Slow",
+    price: 14.99,
+    imageLink: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/41hErtUssNL.jpg"
+  },
+  ]
+  let cont = document.querySelector(".container")
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  let showTotalProduct = document.querySelector(".show");
+  let filter = document.getElementById("filter")
+
+
+filter.addEventListener("change",()=>{
+
+  if(filter.value === ""){
+    display(BadmintonArray)
+  }else{
+    if(filter.value == "assending"){
+      nArray = BadmintonArray.sort((a,b)=>{
+       return a.price - b.price
+      })
+      display(nArray)
+    }else if(filter.value == "descening"){
+      BadmintonArray = BadmintonArray.sort((a,b)=>{
+       return b.price - a.price
+      })
+      display(BadmintonArray)
+    }
+  }
+
+})
+
+
+  function display(data){
+    cont.innerHTML = null;
+    for(let i=0; i<data.length; i++){
+      let card = document.createElement("div")
+      let img = document.createElement("img")
+      img.setAttribute("src",data[i].imageLink) 
+      let des = document.createElement("p")
+      des.innerText = `${data[i].brand}  ${data[i].model}`
+      let price = document.createElement("h4")
+      price.innerText = data[i].price
+      card.append(img,des,price)
+      cont.append(card)
+      showTotalProduct.textContent = i+1
+    }
+
+  }
+  
+display(BadmintonArray)
+
+//-------- search function -----
+
+const searching = ()=>{
+  let search = document.getElementById("search").value.toUpperCase();
+  let cont = document.querySelector(".container")
+  let card = cont.getElementsByTagName("div")
+
+  for(let i=0; i<card.length; i++){
+    let des = card[i].getElementsByTagName("p")[0];
+
+    if(des){
+      let textvalue = des.textContent || des.innerHTML
+
+      if(textvalue.toUpperCase().indexOf(search) > -1){
+        card[i].style.display = "";
+      }else{
+        card[i].style.display = "none";
+      }
+    }
+  }
+}
